df32d67e0b928caeec7e357196570e0974cfc663
Al-Auqash <ilhamtawakhal@gmail.com>
2022-05-10 20:15:27 +0700

    #16

--- resources/js/components/browse/sub/searchBar.js
+++ resources/js/components/browse/sub/searchBar.js
@@ -7,9 +7,7 @@ const categoryOptions = [
     { value: "event", label: "Event" },
 ];
 
-const categoryOptionsStyles = {
-    
-}
+const categoryOptionsStyles = {};
 
 const SearchBar = () => {
     return (
@@ -27,14 +25,39 @@ const SearchBar = () => {
                         </div>
                     </div>
                     <div class="col-4">
-                        <div class="form-group">
-                            <Select
-                                type="text"
-                                class="form-control text-orange"
-                                placeholder="Category"
-                                options={categoryOptions}
-                            />
-                        </div>
+                        {/* <div class="form-group"> */}
+                            <div class="dropdown">
+                                <button
+                                    class="btn btn-secondary dropdown-toggle"
+                                    type="button"
+                                    id="dropdownMenuButton1"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    Dropdown button
+                                </button>
+                                <ul
+                                    class="dropdown-menu"
+                                    aria-labelledby="dropdownMenuButton1"
+                                >
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            Action
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            Another action
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            Something else here
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        {/* </div> */}
                     </div>
                     {/* <div class="col-sm-2">
                         <div class="form-group">
